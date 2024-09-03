ce1af15b25f626a0063dd0e92e0ce081b102f2e6
vipul-rawat <vipul.rawat@gofr.dev>
2024-09-03 11:00:48 +0530

    add service skeleton with GCP deployment

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"gofr.dev/pkg/gofr"
+	gofrSvc "gofr.dev/pkg/gofr/service"
+
+	"kops-deploy-service/client/kops"
+	"kops-deploy-service/handler/deploy"
+	depSvc "kops-deploy-service/service/deploy"
+	"kops-deploy-service/service/upload"
+)
+
+func main() {
+	app := gofr.New()
+
+	kopsClient := gofrSvc.NewHTTPService(app.Config.Get("KOPS_SERVICE_ADDR"), app.Logger(), app.Metrics(),
+		&gofrSvc.APIKeyConfig{APIKey: app.Config.Get("KOPS_API_KEY")})
+
+	kopsSvc := kops.New(kopsClient)
+	uploadSvc := upload.New(kopsSvc)
+	deploySvc := depSvc.New(kopsSvc)
+
+	deployHndlr := deploy.New(uploadSvc, deploySvc)
+
+	app.POST("/deploy", deployHndlr.UploadImage)
+
+	app.Run()
+}
